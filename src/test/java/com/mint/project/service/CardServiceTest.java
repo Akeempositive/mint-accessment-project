51495af97d27d26f83447114da7b5ea18ab3fc0f
akeempositive <akeemsuara@gmail.com>
2020-03-10 23:16:19 +0100

    adding integrated testing for class CardService

---
+++ src/test/java/com/mint/project/service/CardServiceTest.java
@@ -0,0 +1,37 @@
+package com.mint.project.service;
+
+import com.mint.project.model.VerifyCardResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sun.jvm.hotspot.utilities.Assert;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CardServiceTest {
+
+    @Autowired
+    private CardService cardService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void verifyCardByCardNumberFrom3rdPartyApi() {
+        VerifyCardResponse verifyCardResponse = cardService.verifyCardByCardNumberFrom3rdPartyApi("539923");
+        Assert.that(verifyCardResponse.isSuccess(), "The API is not accessible. Third party api not valid/bad network connection");
+        Map<String, String> payload = verifyCardResponse.getPayload();
+        Assert.that(payload.get("scheme") == "Mastercard", "The Scheme of 5399 is no longer mastercard. Probably an outdated result expected");
+        Assert.that(payload.get("bank") == "FBN", "The BIN of First bank is no longer 539923. Expectation and requirements has changed");
+    }
+
+    @Test
+    void getHitCounts() {
+    }
+
+}
