c66e04844888ced84562a4bbbfdd993e6f0f7247
akeempositive <akeemsuara@gmail.com>
2020-03-10 19:48:05 +0100

    first pushed on initiating app

---
+++ src/main/java/com/mint/project/controller/CardSchemeController.java
@@ -0,0 +1,23 @@
+package com.mint.project.controller;
+
+import com.mint.project.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller("card-scheme")
+public class CardSchemeController {
+
+    @Autowired
+    private CardService cardService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "verify/{cardNumber}")
+    public @ResponseBody VerifyCardResponse verifyCard(@PathVariable("cardNumber") String cardNumber){
+        return cardService.verifyCardByCardNumberFrom3rdPartyApi(cardNumber);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "stats")
+    public @ResponseBody HitCountResponse hitCount(@RequestParam("start") int start, @RequestParam("limit") int limit){
+        return cardService.getHitCounts(start, limit);
+    }
+}
---
+++ src/main/java/com/mint/project/service/CardService.java
@@ -0,0 +1,7 @@
+package com.mint.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardService {
+}
